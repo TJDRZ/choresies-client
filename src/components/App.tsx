c92323694a76ef61a5ef04b91f3370936c2f72e2
TJ <tjdrzewiecki@gmail.com>
2023-04-23 19:58:46 -0400

    Header and boilerplate to display it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Menu from "./Menu";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <Menu />
+    </>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <header className="Header">
+      choresies
+      <div></div>
+    </header>
+  );
+}
